e64fe9f53a406e9c08c88c414ec2137a321d0535
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2022-11-15 19:07:53 -0300

    BoardException and defensive programming

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -2,44 +2,65 @@ package boardGame;
 
 public class Board {
 	private int rows;
-	private int columns;
+	private int column;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
-		super();
+		if(rows < 1 || columns <1) {
+			throw new BoardException("Error creating board; there must be at least 1 row and 1 column");
+		}
+	
 		this.rows = rows;
-		this.columns = columns;
+		this.column = columns;
 		pieces = new Piece[rows][columns];
 	}
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 	public int getColumns() {
-		return columns;
-	}
-	public void setColumns(int columns) {
-		this.columns = columns;
+		return column;
 	}
 	
+	
 	public Piece piece(int row, int column) {
+		if(!positionExists(rows, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 		
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is aldready a piece on position" + position);
+		}
 		pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position = position;
 	}
 	
+	public boolean positionExists(int row, int column) {
+	return row>=0 && row < row && column >= 0 && column < column;
+		
+	}
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		return piece(position)!= null;
+	}
+	
+	
 	@Override
 	public String toString() {
-		return "Board [rows=" + rows + ", columns=" + columns + "]";
+		return "Board [rows=" + rows + ", columns=" + column + "]";
 	}
 	
 	
